77b3a3413900cb691b5df1631d9583d261823fb1
Aaron Sutton <aaronjsutton@icloud.com>
2021-07-08 11:06:56 -0400

    Working fade!

--- profile.sh
+++ profile.sh
@@ -1,9 +1,7 @@
 # Copyright (c) 2021 Aaron Joseph Sutton. Licensed under the WTFPL license, version 2.
-
+# shellcheck disable=SC3030
+#
 # manage macOS Terminal.app profiles
-# 
-# INSTALL: #   * add the following to your .bashrc/.zshrc:
-#       . /path/to/profile.sh
 
 [ -d "${_PROFILE_DATA:-$HOME/.term_profiles}" ] && {
   echo "ERROR: profile datafile is a directory."
@@ -28,12 +26,53 @@ _profile() {
     *) break
   esac; [ "$#" -gt 0 ] && shift; done
   # print the current profile if nothing left to do
-  grep "^\*.*" < "$datafile" | cut -c2- | cut -d':' -f1
+  _current_profile | cut -d':' -f1
+}
+
+_current_profile() {
+  grep "^\*.*" < "$datafile" | cut -c2-
+}
+
+__profile_mark() {
+  sed -i '.bak' 's/^*//' "$datafile"
+  sed -i '.bak' -re "s/($1)/*\1/" "$datafile"
 }
 
 _set_profile() {
-  # TODO fade api
-  _osa_exec "profile" "$1"
+  # using br to do floating point division was rather slow, so the interpolation
+  # values are hard-coded.
+  local _t_vals=(0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95)
+
+  local profile=$(echo "$1" | sed 's/-/ /')
+
+  [ "$profile" = "$(_current_profile | cut -d':' -f1)" ] && return;
+ 
+  if [ "$_PROFILE_NOFADE" -eq "0" ]; then
+    # get initial and end text/background values
+    local b0=$(_current_profile | cut -d':' -f2)
+    local b1=$(_osa_exec settings "$profile" "backgroundColor" | sed 's/ //g')
+    local f0=$(_current_profile | cut -d':' -f3)
+    local f1=$(_osa_exec settings "$profile" "normalTextColor" | sed 's/ //g')
+
+    _osa_exec text "$(__profile_lerp3 "$f0" "$f1" 0.5)"
+
+    for t in "${_t_vals[@]}"; do 
+      _osa_exec background "$(__profile_lerp3 "$b0" "$b1" "$t")"
+    done
+  fi
+
+  _osa_exec "profile" "$profile"
+  __profile_mark "$profile"
+}
+
+# poor man's linear interpolation, used to fade
+# comma-separated vectors.
+__profile_lerp3() {
+  for i in $(seq 1 3); do
+    local v0=$(echo "$1" | cut -d',' "-f$i")
+    local v1=$(echo "$2" | cut -d',' "-f$i")
+    echo $(( (1 - $3) * v0 + $3 * v1))
+  done
 }
 
 _osa_exec () {
@@ -47,15 +86,15 @@ _gen_profile_cache () {
     all=$(_osa_exec settings | sed -e 's/,/\n/g')
 
     echo "$all" | while read -r line; do
-       
       name=$(echo "$line" \
       | sed -e 's/ /-/g' \
-      | tr "[:upper:]" "[:lower:]" \
-      | sed -e "s/\(${current}\)/*\1/")
+      | tr "[:upper:]" "[:lower:]")
 
       colors=$(echo "$(_osa_exec settings "$line" backgroundColor):$(_osa_exec settings "$line" normalTextColor)" | sed 's/ //g')
       echo "$name:$colors"
+
     done
+    __profile_mark "$current"
 }
 
 # locate the JXA script file
@@ -67,5 +106,4 @@ fi
 export _PROFILE_SCRIPT_LIBRARY;
 
 # expansion intended for this alias
-# shellcheck disable=SC2139
 alias "${_PROFILE_CMD:-profile}"='_profile 2>&1'
